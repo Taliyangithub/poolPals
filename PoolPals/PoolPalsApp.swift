1d1beeb12b6ae7b39349cbd555ae7519121f2378
Priya Taliyan <chaudharypia94@gmail.com>
2025-12-30 22:22:39 +0100

    Initial Commit

---
+++ PoolPals/PoolPalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoolPalsApp.swift
+//  PoolPals
+//
+//  Created by Priya Taliyan on 2025-12-30.
+//
+
+import SwiftUI
+
+@main
+struct PoolPalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
